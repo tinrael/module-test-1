75d67088b229740dbb3ef3892fff3d082d4df7c7
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2019-10-26 22:12:33 +0300

    Add examples of usage

--- task2/task2/task2.cpp
+++ task2/task2/task2.cpp
@@ -1,20 +1,47 @@
-﻿// task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include "include/ComputeFunction.h"
 #include <iostream>
+#include <utility>
+#include <vector>
+
+using std::cout;
+using std::endl;
+
+void printText(char* text) {
+	int i = 0;
+	while (text[i] != '\0') {
+		cout << text[i];
+		i++;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n = 5;
+	cout << "Input: " << n << " | Result: " << f(n) << endl;
+	
+	n = -2;
+	cout << "Input: " << n << " | Result: " << f(n) << endl;
+	
+	double d = 2.3;
+	cout << "Input: " << d << " | Result: " << f(d) << endl;
+	
+	char symbols[] = "ab#56bSh";
+	cout << "Input: ";
+	printText(symbols);
+	cout << " | Result: " << f(symbols) << endl;
+	
+	pair<int, double> myPair(3, 2.3);
+	cout << "Input: (" << myPair.first << ", " << myPair.second << ") | Result: " << f(&myPair)
+		<< endl;
+
+	vector<int> v;
+	v.push_back(5);
+	v.push_back(-3);
+	v.push_back(7);
+	v.push_back(2);
+	cout << "Input: ";
+	for (size_t i = 1; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+	cout << " | Result: " << f(v);
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
