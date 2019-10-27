052ccfdb6e347d940e7b60178fd364963f05562d
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2019-10-27 17:34:16 +0300

    Add static data member, pure virtual destructor (make class Base be abstract) and method getId (generate unique id for each object)

--- task3/task3/include/Base.cpp
+++ task3/task3/include/Base.cpp
@@ -1,1 +1,16 @@
 #include "Base.h"
+
+int Base::n = 1;
+
+Base::Base() {
+	id = n;
+	n++;
+}
+
+Base::~Base() {
+
+}
+
+int Base::getId() {
+	return id;
+}
--- task3/task3/include/Base.h
+++ task3/task3/include/Base.h
@@ -1,6 +1,13 @@
 #pragma once
 
 class Base {
+public:
+	Base();
+	virtual ~Base() = 0;
+	int getId();
 
+protected:
+	static int n;
+	int id = 0;
 };
 
