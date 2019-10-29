328506b0beabf6eef9b6f7ce29ba306be4b498ba
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2019-10-29 20:10:59 +0200

    Add example of usage

---
+++ task3/task3/task3.cpp
@@ -0,0 +1,22 @@
+﻿#include "include/Alpha.h"
+#include "include/Beta.h"
+#include "include/Gamma.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	Base* a = new Alpha();
+	Base* b = new Beta();
+	Base* c = new Gamma();
+	Base* d = new Alpha();
+	vector<Base*> objects{a, b, c, d};
+	cout << "Predict S before deletion: " << Base::predictS(objects) << endl;
+	delete a;
+	delete b;
+	delete c;
+	delete d;
+	cout << "S after deletion all objects: " << Base::getS() << endl;
+}
