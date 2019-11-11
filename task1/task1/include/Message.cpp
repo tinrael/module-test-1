ab078994d9d80dbc3ce4a57ce770817f77f2653f
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2019-11-11 20:39:15 +0200

    Add constructors, method addTheme and data members

--- task1/task1/include/Message.cpp
+++ task1/task1/include/Message.cpp
@@ -1,1 +1,13 @@
 #include "Message.h"
+
+Message::Message(Theme* theme, User* author, char* text) : rating(0), author(author), text(text) {
+	addTheme(theme);
+}
+
+Message::Message(vector<Theme*> themes, User* author, char* text) : rating(0), author(author), text(text) {
+	this->themes = themes;
+}
+
+void Message::addTheme(Theme* theme) {
+	this->themes.push_back(theme);
+}
--- task1/task1/include/Message.h
+++ task1/task1/include/Message.h
@@ -1,7 +1,22 @@
 #pragma once
+
+#include <vector>
 #include "User.h"
+#include "Theme.h"
+
+using std::vector;
+
 class Message {
 private:
-	User& author;
+	vector<Theme*> themes;
+	char* text;
+	User* author;
+	int rating;
+
+public:
+	Message(Theme* theme, User* author, char* text);
+	Message(vector<Theme*> themes, User* author, char* text);
+
+	void addTheme(Theme* theme);
 };
 
