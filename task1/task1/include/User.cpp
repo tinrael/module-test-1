285054a55e8af4c3772ba0f12d052c71d6929feb
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2019-11-12 09:31:49 +0200

    Add getters, data members and methods addFriend, postMessage, isFriend

--- task1/task1/include/User.cpp
+++ task1/task1/include/User.cpp
@@ -1,11 +1,43 @@
 #include "User.h"
 
-User::User(string name, int age) {
+User::User(string name, int age, vector<Topic*> favoriteTopics) {
 	this->name = name;
 	this->age = age;
+	this->favoriteTopics = favoriteTopics;
 }
 
 User::~User() {
+	for (Message* message : this->messages) {
+		delete message;
+	}
+}
+
+bool User::isFriend(User* userToCheck) {
+	for (User* currentUser : this->friends) {
+		if (currentUser == userToCheck) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void User::addFriend(User* newFriend) {
+	if (this->isFriend(newFriend)) {
+		return;
+	}
+	this->friends.push_back(newFriend);
+}
+
+void User::postMessage(Message* message) {
+	this->messages.push_back(message);
+}
+
+vector<User*> User::getFriends() {
+	return this->friends;
+}
+
+vector<Topic*> User::getFavoriteTopics() {
+	return this->favoriteTopics;
 }
 
 string User::getName() {
@@ -15,5 +47,3 @@ string User::getName() {
 int User::getAge() {
 	return age;
 }
-
-
--- task1/task1/include/User.h
+++ task1/task1/include/User.h
@@ -1,18 +1,32 @@
 #pragma once
+
+#include "Topic.h"
 #include <string>
+#include <vector>
 
 using std::string;
+using std::vector;
+
+class Message;
 
 class User {
 private:
 	string name;
 	int age;
-	User* friends[10];
+	vector<User*> friends;
+	vector<Message*> messages;
+	vector<Topic*> favoriteTopics;
 
 public:
-	User(string name, int age);
-	virtual ~User();	
+	User(string name, int age, vector<Topic*> favoriteTopics);
+	virtual ~User();
+
+	bool isFriend(User* userToCheck);
+	void addFriend(User* newFriend);
+	void postMessage(Message* message);
+	
+	vector<User*> getFriends();
+	vector<Topic*> getFavoriteTopics();
 	string getName();
 	int getAge();
 };
-
