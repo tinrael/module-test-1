33e37f214fc3324cd7ba58de3f2e4ce5d7ed15bf
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2019-11-12 09:26:49 +0200

    Add methods react, repost addMentions and add getters

--- task1/task1/include/Message.cpp
+++ task1/task1/include/Message.cpp
@@ -1,19 +1,67 @@
 #include "Message.h"
 
-Message::Message(Theme* theme, User* author, char* text) 
-	: rating(0), author(author), text(text) {
-	addTheme(theme);
-}
-
-Message::Message(vector<Theme*> themes, User* author, char* text) 
-	: rating(0), author(author), text(text) {
-	this->themes = themes;
-}
+Message::Message(vector<Topic*> topics, User* author, string text, Message* replyFor)
+	: topics(topics), rating(0), author(author), text(text), replyFor(replyFor) {}
 
 Message::~Message() {
-	delete text;
+	for (Reaction* reaction : this->reactions) {
+		delete reaction;
+	}
 }
 
-void Message::addTheme(Theme* theme) {
-	this->themes.push_back(theme);
+void Message::addMention(User* user) {
+	this->mentions.push_back(user);
+}
+
+void Message::addMentions(vector<User*> users) {
+	for (User* user : users) {
+		this->addMention(user);
+	}
+}
+
+bool Message::hasReacted(User* userToCheck) {
+	for (Reaction* reaction : this->reactions) {
+		if (reaction->user == userToCheck) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void Message::react(User* user, ReactionType type) {
+	if (this->hasReacted(user)) {
+		return;
+	}
+
+	int reactionValue;
+	if (type == LIKE) {
+		reactionValue = 1;
+	}
+	else {
+		reactionValue = -1;
+	}
+	this->reactions.push_back(new Reaction(reactionValue, user));
+	this->rating += reactionValue;
+}
+
+Message* Message::repost(User* author) {
+	return this->repost(author, vector<User*>());
+}
+
+Message* Message::repost(User* newAuthor, vector<User*> additionalMentions) {
+	Message* result = new Message(this->topics, newAuthor, this->text);
+
+	result->addMentions(this->mentions);
+	result->addMentions(additionalMentions);
+
+	return result;
+}
+
+vector<Reaction*> Message::getReactions() {
+	return this->reactions;
+}
+
+int Message::getRating() {
+	return this->rating;
 }
--- task1/task1/include/Message.h
+++ task1/task1/include/Message.h
@@ -1,23 +1,35 @@
 #pragma once
 
-#include <vector>
 #include "User.h"
-#include "Theme.h"
+#include "Reaction.h"
+#include "Topic.h"
+#include <string>
+#include <vector>
 
+using std::string;
 using std::vector;
 
 class Message {
 private:
-	vector<Theme*> themes;
-	char* text;
+	vector<Topic*> topics;
+	string text;
 	User* author;
 	int rating;
+	Message* replyFor;
+	vector<Reaction*> reactions;
+	vector<User*> mentions;
 
 public:
-	Message(Theme* theme, User* author, char* text);
-	Message(vector<Theme*> themes, User* author, char* text);
+	Message(vector<Topic*> topics, User* author, string text, Message* replyFor = nullptr);
 	virtual ~Message();
 
-	void addTheme(Theme* theme);
+	void addMention(User* user);
+	void addMentions(vector<User*> users);
+	bool hasReacted(User* userToCheck);
+	void react(User* user, ReactionType type);
+	Message* repost(User* newAuthor);
+	Message* repost(User* newAuthor, vector<User*> additionalMentions);
+	
+	vector<Reaction*> getReactions();
+	int getRating();
 };
-
